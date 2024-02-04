dc8a1b5c83a13b3c15b648d0d22c5d997965aae2
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2024-02-03 19:26:31 -0600

    creating router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { BlogPosts } from "./BlogPosts";
+import './App.css';
+import { Root } from "./Root";
+import Footer from './Footer'; // replace with your actual import
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [count, setCount] = useState(0); // Initialize count state variable
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path = "/" element={<Root/>}>
+        <Route index element={<BlogPosts/>}/>
 
+      </Route>
+  )
+  );
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router}>
+        <div>
+          <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <Footer />
+      </RouterProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
